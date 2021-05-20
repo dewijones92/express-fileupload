bea6efa90737e23f28206cea277dbbe11494165a
Dewi Jones <dewijones92@gmail.com>
2021-05-20 10:06:14 +0100

    more

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x;
-BASEPATH="/home/dewi/Downloads";
+BASEPATH="$HOME/Downloads";
 source deepspeech/bin/activate &>/dev/null;
 deepspeech --model $BASEPATH/deepspeech-0.9.3-models.pbmm --scorer $BASEPATH/deepspeech-0.9.3-models.scorer   --audio $1;
